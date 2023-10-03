e4c6ca64622eeb28ef849e47b72c71c593a15eb9
Sergio Granjo <sgranjot@gmail.com>
2023-10-03 12:00:54 +0200

    primer commit

---
+++ src/main/java/com/sgranjot/alquilao/backend/alquilao/AlquilaoApplication.java
@@ -0,0 +1,13 @@
+package com.sgranjot.alquilao.backend.alquilao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgranjot/alquilao/backend/alquilao/AlquilaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgranjot.alquilao.backend.alquilao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
